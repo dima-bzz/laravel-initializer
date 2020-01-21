f2f147db0164eb714f92e3511bf5f24ed9327a45
Dmitry Mazurov <dimabzz@gmail.com>
2020-01-21 16:45:30 +0300

    Added testcase for options.
    Signed-off-by: Dmitry Mazurov <dimabzz@gmail.com>

---
+++ tests/OptionRunnerCommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MadWeb\Initializer\Test;
+
+use MadWeb\Initializer\Run;
+
+class OptionRunnerCommandTest extends RunnerCommandsTestCase
+{
+
+    /**
+     * @test
+     * @dataProvider initCommandsOptionSet
+     */
+    public function option($command)
+    {
+        $test = false;
+
+        $this->declareCommands(function (Run $run) use (&$test) {
+            $test = $run->getOption('some');
+        }, $command);
+
+        $this->assertTrue($test);
+    }
+}
