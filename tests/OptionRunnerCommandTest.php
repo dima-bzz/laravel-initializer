41452c4e9ec77991a2e74b28af9157db2d34a940
Dmitry Mazurov <dimabzz@gmail.com>
2020-01-22 13:15:19 +0300

    Added new testcase
    Signed-off-by: Dmitry Mazurov <dimabzz@gmail.com>

--- tests/OptionRunnerCommandTest.php
+++ tests/OptionRunnerCommandTest.php
@@ -10,7 +10,7 @@ class OptionRunnerCommandTest extends RunnerCommandsTestCase
      * @test
      * @dataProvider initCommandsOptionSet
      */
-    public function option($command)
+    public function option_set($command)
     {
         $test = false;
 
@@ -19,5 +19,35 @@ class OptionRunnerCommandTest extends RunnerCommandsTestCase
         }, $command);
 
         $this->assertTrue($test);
+    }
+
+    /**
+     * @test
+     * @dataProvider initCommandsOptionSet
+     */
+    public function option_wrong($command)
+    {
+        $test = false;
+
+        $this->declareCommands(function (Run $run) use (&$test) {
+            $test = $run->getOption('wrong');
+        }, $command);
+
+        $this->assertFalse($test);
+    }
+
+    /**
+     * @test
+     * @dataProvider initCommandsSet
+     */
+    public function option_not_set($command)
+    {
+        $test = false;
+
+        $this->declareCommands(function (Run $run) use (&$test) {
+            $test = $run->getOption('some');
+        }, $command);
+
+        $this->assertFalse($test);
     }
 }
